3ad8233db9bc156f970b3381e2773a4bd179a920
Davis <davis@wizmacau.com>
2020-03-05 22:10:47 +0800

    update

---
+++ src/network/request.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+export function request(config) {
+
+  // 创建axios 实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000/api/wh',
+    // baseURL: http://106.54.54.237:8000/api/wh ,
+    timeout: 5000
+  });
+
+  // 请求拦截
+  instance.interceptors.request.use(config => {
+    return config;
+  }, error => {
+    console.log(error);
+  });
+
+  //响应拦截
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, error => {
+    console.log(error);
+  });
+
+  return instance(config);
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,46 @@
 <template>
-  <h2>Home 首页</h2>
-
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "./childComps/HomeSwiper";
+
+  import {getHomeMultiData} from "network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    data() {
+      return {
+        banner: [],
+        recommend: [],
+        dKeyword: [],
+        keywords: [],
+      }
+    },
+    created() {
+      getHomeMultiData().then(res => {
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+        this.dKeyword = res.data.dKeyword.list;
+        this.keywords = res.data.keywords.list;
+      })
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    color: #ffffff;
+    background-color: var(--color-tint);
+  }
 </style>
